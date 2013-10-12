57aeba880c69cfb3853454b756e3f37940a29dbe
Michael Bridgen <mikeb@squaremobius.net>
2013-10-12 17:05:10 +0100

    Signpost test cases

--- test-exprs.scm
+++ test-exprs.scm
@@ -1,3 +1,13 @@
+;; Test expressions for run-smoketest.scm
+
+"built-in in head position, literals"
 (+ 1 1)
+
+"let form"
 ((lambda (x y) (+ y x)) 1 2)
-((lambda (f . args) (apply f args)) + 1 3)
+
+"abstraction and application"
+((lambda (f) (f 2 2)) (lambda (x y) (+ x y)))
+
+"dotted let, built-in as value, and apply"
+((lambda (f . args) (apply f args)) + 2 3)
